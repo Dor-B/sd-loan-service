39ac2863cf2324b2c1d2f579074b10be4850c248
Miki Mints <mikimn1999@gmail.com>
2021-05-16 02:51:59 +0300

    Update to CompletableFuture

--- src/main/kotlin/il/ac/technion/cs/softwaredesign/execution/ExecutionService.kt
+++ src/main/kotlin/il/ac/technion/cs/softwaredesign/execution/ExecutionService.kt
@@ -1,6 +1,8 @@
 @file:Suppress("unused", "UNUSED_PARAMETER")
 package il.ac.technion.cs.softwaredesign.execution
 
+import java.util.concurrent.CompletableFuture
+
 
 interface ExecutionService {
     /**
@@ -11,11 +13,11 @@ interface ExecutionService {
      * @throws IllegalArgumentException If the system failed to allocate this resource. This means that it either
      * could not be detected (down or disconnected) or it is already allocated by a different thread.
      */
-    fun allocateResource(id: String): GeneralResource
+    fun allocateResource(id: String): CompletableFuture<GeneralResource>
 
     /**
      * Signals that the calling thread releases this resource and it is subsequently returned to the operating system.
      * Subsequent calls to [allocateResource] see this resource as available in the system.
      */
-    fun releaseResource(resource: GeneralResource)
+    fun releaseResource(resource: GeneralResource): CompletableFuture<Unit>
 }
--- src/main/kotlin/il/ac/technion/cs/softwaredesign/execution/impl/ExecutionServiceImpl.kt
+++ src/main/kotlin/il/ac/technion/cs/softwaredesign/execution/impl/ExecutionServiceImpl.kt
@@ -2,9 +2,10 @@ package il.ac.technion.cs.softwaredesign.execution.impl
 
 import il.ac.technion.cs.softwaredesign.execution.ExecutionService
 import il.ac.technion.cs.softwaredesign.execution.GeneralResource
+import java.util.concurrent.CompletableFuture
 
 class ExecutionServiceImpl : ExecutionService {
-    override fun allocateResource(id: String): GeneralResource = throw NotImplementedError()
+    override fun allocateResource(id: String): CompletableFuture<GeneralResource> = throw NotImplementedError()
 
-    override fun releaseResource(resource: GeneralResource) = throw NotImplementedError()
+    override fun releaseResource(resource: GeneralResource): CompletableFuture<Unit> = throw NotImplementedError()
 }
