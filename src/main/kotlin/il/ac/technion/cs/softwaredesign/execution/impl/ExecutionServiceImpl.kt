635a62dff10dbfdca9c4ea8999888a0d79ad6a0f
Miki Mints <mikimn1999@gmail.com>
2021-05-29 23:30:48 +0300

    fix

--- src/main/kotlin/il/ac/technion/cs/softwaredesign/execution/impl/ExecutionServiceImpl.kt
+++ src/main/kotlin/il/ac/technion/cs/softwaredesign/execution/impl/ExecutionServiceImpl.kt
@@ -5,7 +5,7 @@ import il.ac.technion.cs.softwaredesign.execution.GeneralResource
 import java.util.concurrent.CompletableFuture
 
 class ExecutionServiceImpl : ExecutionService {
-    override fun verifyResource(id: String, expectedType: Class<GeneralResource>): CompletableFuture<Boolean> = throw NotImplementedError()
+    override fun verifyResource(id: String): CompletableFuture<Class<GeneralResource>> = throw NotImplementedError()
 
     override fun allocateResource(id: String): CompletableFuture<GeneralResource> = throw NotImplementedError()
 
